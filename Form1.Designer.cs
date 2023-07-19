ecdb4050a53a71d4bea1e6620c1605b56816a774
Leonardo Moreira <eng.leonardomoreira@outlook.com>
2023-07-19 02:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Estudo02
+{
+    partial class FrmFormulario
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota01 = new System.Windows.Forms.TextBox();
+            this.txtNota02 = new System.Windows.Forms.TextBox();
+            this.txtNota03 = new System.Windows.Forms.TextBox();
+            this.txtNota04 = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNota01
+            // 
+            this.txtNota01.Location = new System.Drawing.Point(0, 0);
+            this.txtNota01.Name = "txtNota01";
+            this.txtNota01.Size = new System.Drawing.Size(100, 20);
+            this.txtNota01.TabIndex = 0;
+            // 
+            // txtNota02
+            // 
+            this.txtNota02.Location = new System.Drawing.Point(0, 0);
+            this.txtNota02.Name = "txtNota02";
+            this.txtNota02.Size = new System.Drawing.Size(100, 20);
+            this.txtNota02.TabIndex = 0;
+            // 
+            // txtNota03
+            // 
+            this.txtNota03.Location = new System.Drawing.Point(0, 0);
+            this.txtNota03.Name = "txtNota03";
+            this.txtNota03.Size = new System.Drawing.Size(100, 20);
+            this.txtNota03.TabIndex = 0;
+            // 
+            // txtNota04
+            // 
+            this.txtNota04.Location = new System.Drawing.Point(0, 0);
+            this.txtNota04.Name = "txtNota04";
+            this.txtNota04.Size = new System.Drawing.Size(100, 20);
+            this.txtNota04.TabIndex = 0;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Location = new System.Drawing.Point(0, 0);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(75, 23);
+            this.btnResultado.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nota 1º Bimestre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nota 2º Bimestre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nota 3º Bimestre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nota 4º Bimestre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Média das notas anual";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(203, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Insira os dados abaixo";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(191, 59);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(210, 20);
+            this.txtNota1.TabIndex = 12;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(191, 105);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(210, 20);
+            this.txtNota2.TabIndex = 13;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(191, 150);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(210, 20);
+            this.txtNota3.TabIndex = 14;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(191, 196);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(210, 20);
+            this.txtNota4.TabIndex = 15;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(191, 275);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(210, 20);
+            this.txtResultado.TabIndex = 16;
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedia.Location = new System.Drawing.Point(191, 222);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(210, 47);
+            this.btnMedia.TabIndex = 17;
+            this.btnMedia.Text = "Calcular Média \r\n\r\n";
+            this.btnMedia.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMedia.UseVisualStyleBackColor = false;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // FrmFormulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(500, 370);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFormulario";
+            this.Text = "Média das Notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota01;
+        private System.Windows.Forms.TextBox txtNota02;
+        private System.Windows.Forms.TextBox txtNota03;
+        private System.Windows.Forms.TextBox txtNota04;
+        private System.Windows.Forms.TextBox txtResMedia;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnMedia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudo02
+{
+    public partial class FrmFormulario : Form
+    {
+        public FrmFormulario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            int nota01;
+            int nota02;
+            int nota03;
+            int nota04;
+            int mediaNotas;
+
+            nota01 = int.Parse(txtNota1.Text);
+            nota02 = int.Parse(txtNota2.Text);
+            nota03 = int.Parse(txtNota3.Text);
+            nota04 = int.Parse(txtNota4.Text);
+
+            mediaNotas = nota01 + nota02 + nota03 + nota04 / 4;
+
+            txtResultado.Text = Convert.ToString(mediaNotas);
+        }
+    }
+}
